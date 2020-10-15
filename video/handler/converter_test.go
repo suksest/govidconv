3d9d199e8f67f3e8879abade496d6c69ff0f292b
ridwanfathin <m.ridwanfathin@gmail.com>
2020-10-15 16:40:33 +0700

    Add handler unit test: bad request

--- video/handler/converter_test.go
+++ video/handler/converter_test.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
 	"gitlab.com/prosa-backend-test/video_converter/domain"
 	"gitlab.com/prosa-backend-test/video_converter/domain/mocks"
 )
@@ -30,5 +34,22 @@ func TestNewVideoHandler(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			NewVideoHandler(tt.args.e, tt.args.vu)
 		})
+	}
+}
+
+func TestConvert(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/video/convert", strings.NewReader("test"))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := &VideoHandler{}
+
+	// Assertions
+	// test
+	if assert.NoError(t, h.Convert(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, "\"request Content-Type isn't multipart/form-data\"\n", rec.Body.String())
 	}
 }
